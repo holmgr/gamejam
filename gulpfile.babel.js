dc5f298876ef82e1c130374c1089da5caf03d801
Axel Nordanskog <axeno840@student.liu.se>
2019-09-14 20:30:26 +0200

    Prevent removal of unused font-faces
    Since this will also remove font-faces that are used but only in other
    CSS files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -95,7 +95,7 @@ gulp.task('styles', () => {
     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe(gulp.dest('.tmp/styles'))
     // Concatenate and minify styles
-    .pipe($.if('*.css', $.cssnano()))
+    .pipe($.if('*.css', $.cssnano({discardUnused: {fontFace: false}})))
     .pipe($.size({title: 'styles'}))
     .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest('dist/styles'));
